b84726782d6af4164441e747388f47319e4302e5
afreen shaik <sk.afreen12@gmail.com>
2019-08-21 09:59:33 +0530

    authentication poc

---
+++ src/main/java/com/stackroute/SpringBootSuccourApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSuccourApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBootSuccourApplication.class, args);
+	}
+}
